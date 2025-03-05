fd22f829b52b3381adf9863221c1b51edf05078a
Justin Kline <50549915+justakline@users.noreply.github.com>
2025-03-05 20:38:17 +0000

    Added The test cases to follow TDD

--- README.md
+++
@@ -1,3 +0,0 @@
-# Travis412
-
-[![Build Status](https://travis-ci.com/dmark1021/Travis412.svg?branch=master)](https://travis-ci.com/dmark1021/Travis412)
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,7 +9,7 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
+		assertEquals(calc.add(1, 1), 2);
 	}
 
 	@Test
@@ -17,4 +17,42 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(1, 1), 0);
 	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2, 3), 6);
+	}
+
+	@Test
+	public void testMultiplyByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(0, 3), 0);
+	}
+
+	@Test
+	public void testMultiplyByOne() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(1, 3), 1);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(5, 2), 2);
+	}
+
+	@Test
+	public void testDivideByLargerNumber() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(2,5), 0);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testDivideByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.divide(5, 0);
+	}
+
+
 }
